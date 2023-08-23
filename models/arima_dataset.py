1a432519e2ea3108558ded75bbee52d60cfbdcce
racynniv <racynniv@gmail.com>
2023-08-23 12:19:44 -0400

    Reorg

--- models/arima_dataset.py
+++ models/arima_dataset.py
@@ -7,6 +7,8 @@ import numpy as np
 weather = os.listdir('data/WeatherStations')
 danger = pd.read_csv('data/ratings_comb.csv')
 
+times = 
+
 for i,r in danger.iterrows():
     yr = r['Year']
     mo = r['Month']
